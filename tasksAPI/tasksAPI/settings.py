b87477f81d66acc1b1c38043df9bb3fbc34f2ffe
sydneyIdundun <sydney.idundun@gmail.com>
2022-12-21 21:24:46 +0400

    feat: Added token based authentication
    Added JWT authentication using simplejwt, and added two routes for obtaining tokens and refreshing tokens

--- tasksAPI/tasksAPI/settings.py
+++ tasksAPI/tasksAPI/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Users',
+
+    # third party apps
+    'rest_framework',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +128,11 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'Users.CustomUser'
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
+}
--- tasksAPI/tasksAPI/urls.py
+++ tasksAPI/tasksAPI/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from rest_framework_simplejwt.views import (TokenRefreshView, TokenObtainPairView)
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/token', TokenObtainPairView.as_view(), name="obtain-token"),
+    path("auth/refresh", TokenRefreshView.as_view(), name="refresh-token")
 ]
