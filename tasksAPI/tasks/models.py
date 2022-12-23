fba1d68422b43b41af6d47ff0bd5688919301a8d
sydneyIdundun <sydney.idundun@gmail.com>
2022-12-23 09:01:17 +0400

    chore: Created Collections and Tasks models

--- tasksAPI/tasks/models.py
+++ tasksAPI/tasks/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+class Collections(models.Model):
+    id = models.UUIDField(default=uuid4(), primary_key=True)
+    title = models.CharField(max_length=20)
+    description = models.CharField(max_length=100, blank=True, null=True)
+    archived = models.BooleanField(default=False)
+    completed = models.IntegerField(default=0)
+    total_tasks = models.IntegerField(default=0)
+
+    @property
+    def progress(self):
+        return (self.completed/self.total_tasks) * 100
+
+    def __str__(self) -> str:
+        return self.title.capitalize()
+
+class Tasks(models.Model):
+    _id = models.AutoField()
+    collection = models.ForeignKey(Collections)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=100, blank=True, null=True)
+    completed = models.BooleanField(default=False)
+
+    @property
+    def id(self):
+        return str(self.collections + self.id)
+
+
+    def __str__(self) -> str:
+        return self.title
